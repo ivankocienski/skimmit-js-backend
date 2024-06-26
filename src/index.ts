ad9ce9f07959705f058989693f59c84b663357eb
Ivan Kocienski <ivan@gfsc.studio>
2024-06-26 15:30:09 +0100

    Pug views (and basic CSS reset)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
+import path from 'path';
 import express from 'express';
 import * as config from './Config';
 
 const app = express();
 
+app.set('view engine', 'pug');
+
+const viewsDir = path.join(__dirname, 'views');
+console.log("viewsDir=", viewsDir);
+app.set('views', viewsDir);
+
+const staticDir = path.join(__dirname, 'public');
+console.log("staticDir=", staticDir);
+app.use(express.static(staticDir));
+
 app.get('/', (req, res) => {
-  res.send({ message: "Hello there "});
+  res.render('index');
+});
+
+app.use((_req, res, _next) => {
+  res.status(404).render('not_found');
 });
 
 app.listen(config.serverPort(), () => {
